95ce4130740f5af7c4fcd5e1ebe3f29ab9ab2c04
Ian Strouse <ian.strouse@gmail.com>
2016-06-14 19:46:44 -0600

    add to seed

--- bathroom-finder-app/seeds/firstSeed.js
+++ bathroom-finder-app/seeds/firstSeed.js
@@ -30,24 +30,69 @@ exports.seed = function(knex, Promise) {
       lng: -105.2833636,
       users_id: 1,
       directions: 'Go though yellow deli, out the back, down hallway to the right'
-    })
+    }),
     knex('bathrooms').insert({
       id: 4,
       bathroomname: 'Hallway behind Zoe Ma Ma',
       rating: 3,
-      lat: 40.0168749,
-      lng: -105.2833636,
+      lat: 40.01774439,
+      lng: -105.28240096,
       users_id: 1,
-      directions: 'Go though yellow deli, out the back, down hallway to the right'
-    })
+      directions: 'Go into Zoe Ma Ma, past restaurant and down the hall to the left'
+    }),
     knex('bathrooms').insert({
       id: 5,
-      bathroomname: 'Hallway behind Yellow Deli',
+      bathroomname: 'Pearl Street Pub',
       rating: 2,
-      lat: 40.0168749,
-      lng: -105.2833636,
+      lat: 40.01727193,
+      lng: -105.28079834,
       users_id: 1,
-      directions: 'Go though yellow deli, out the back, down hallway to the right'
+      directions: 'Just past back entrance to Pearl Street Pub'
+    }),
+    knex('bathrooms').insert({
+      id: 6,
+      bathroomname: 'Hallway behind Chipotle/World of Beer',
+      rating: 2,
+      lat: 40.0174907,
+      lng: -105.2833907,
+      users_id: 1,
+      directions: 'Go though either Chipotle or WoB, no one cares'
+    }),
+    knex('bathrooms').insert({
+      id: 7,
+      bathroomname: '29th Street Mall',
+      rating: 2,
+      lat: 40.01869805,
+      lng: -105.25531724,
+      users_id: 1,
+      directions: 'Take staircase between Teavana and Eddie Bauer, turn right at the balcony and go all the way around'
+    }),
+    knex('bathrooms').insert({
+      id: 8,
+      bathroomname: 'McDonald\'s',
+      rating: 2,
+      lat: 40.01930915,
+      lng: -105.25783114,
+      users_id: 1,
+      directions: 'Go inside, no one cares'
+    }),
+    knex('bathrooms').insert({
+      id: 9,
+      bathroomname: 'Target',
+      rating: 2,
+      lat: 40.02206674,
+      lng: -105.2566731,
+      users_id: 1,
+      directions: 'Just inside on the left, no one cares'
+    }),
+    knex('bathrooms').insert({
+      id: 10,
+      bathroomname: 'Hallway behind Chipotle/World of Beer',
+      rating: 2,
+      lat: 39.99876598,
+      lng: -105.28250009,
+      users_id: 1,
+      directions: 'Around the left of the ranger\'s station at Chautauqua'
     })
   );
 };
